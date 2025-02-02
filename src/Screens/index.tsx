ba70143c541f44433ff80928cc77d9874b71fe5e
sercan turkoglu <sercanturkoglu67@gmail.com>
2025-02-02 15:15:03 +0000

    feat(): Text input

--- src/Screens/index.tsx
+++ src/Screens/index.tsx
@@ -1,18 +1,64 @@
-import {StyleSheet, Text, View} from 'react-native';
-import React from 'react';
+import {StyleSheet, Text, TextInput, View} from 'react-native';
+import React, {useState} from 'react';
 import {ShoppingListItem} from '../components/ShoppingListItem';
 import {Link} from '@react-navigation/native';
+import {theme} from '../theme';
+
+type ShoppingListItemType = {
+  id: string;
+  name: string;
+  isCompleted?: boolean;
+};
+
+const initialList: ShoppingListItemType[] = [
+  {
+    id: '1',
+    name: 'Coffee',
+  },
+  {
+    id: '2',
+    name: 'Tee',
+    isCompleted: true,
+  },
+  {
+    id: '3',
+    name: 'Sugar',
+    isCompleted: true,
+  },
+];
 
 const Home = (): React.JSX.Element => {
+  const [value, setValue] = useState('');
+  const [list, setList] = useState<ShoppingListItemType[]>(initialList);
+
+  const handleSubmit = () => {
+    if (value) {
+      setList(prevState => {
+        return [{name: value, id: new Date().toDateString()}, ...prevState];
+      });
+      setValue('');
+    }
+  };
   return (
     <View style={styles.container}>
       <Link screen="Counter" style={styles.link}>
         <Text>Go to Counter</Text>
       </Link>
-
-      <ShoppingListItem name="coffee" />
-      <ShoppingListItem name="tee" isCompleted />
-      <ShoppingListItem name="Suger" isCompleted />
+      <TextInput
+        value={value}
+        style={styles.TextInput}
+        placeholder="E.g Coffee"
+        onChangeText={setValue}
+        returnKeyType="done"
+        onSubmitEditing={handleSubmit}
+      />
+      {list.map(item => (
+        <ShoppingListItem
+          name={item.name}
+          key={item.id}
+          isCompleted={item.isCompleted}
+        />
+      ))}
     </View>
   );
 };
@@ -21,12 +67,20 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: '#fff',
-    justifyContent: 'center',
   },
   link: {
     textAlign: 'center',
     marginBottom: 18,
     fontSize: 24,
+  },
+  TextInput: {
+    borderColor: theme.colorLightGrey,
+    borderWidth: 2,
+    padding: 12,
+    marginHorizontal: 12,
+    marginBottom: 12,
+    fontSize: 16,
+    borderRadius: 15,
   },
 });
 export default Home;
